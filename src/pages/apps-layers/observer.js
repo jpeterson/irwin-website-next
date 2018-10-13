132afc67f79bea64363e0f05ae90d12bd5fa1664
Joshua Peterson <jpeterson@esri.com>
2018-10-13 11:14:56 -0600

    basic example of "query posts by tag" graphql query... still needs some abstraction

--- src/pages/apps-layers/observer.js
+++ src/pages/apps-layers/observer.js
@@ -1,13 +1,52 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../../components/layout';
+import PostsByTag from '../../components/PostsByTag';
 
-const ObserverPage = () => {
+const ObserverPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges.map(edge => edge.node);
+
   return (
     <Layout>
       <div>Observer homepage</div>
+      <hr />
+      <div>
+        <h4>See the latest Observer posts:</h4>
+        <PostsByTag tag="Observer" posts={posts} />
+      </div>
     </Layout>
   );
 };
+
+// Working example of a graphql query to pull out posts by tag... need
+// to figure out how to do this more dynamically (i.e. pass tag name to React
+// component PostsByTag and let it query)
+export const pageQuery = graphql`
+  query ObserverPostsQuery {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: ASC }
+      filter: {
+        frontmatter: { tags: { elemMatch: { tag: { in: ["Observer"] } } } }
+      }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+            date
+            author
+            tags {
+              tag
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
 
 export default ObserverPage;
